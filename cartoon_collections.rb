ca4574b0d4e8496bd88fa49f4e36549962e859c1
Marie Burns <mcburns2222@gmail.com>
2018-11-20 16:43:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 
 def summon_captain_planet(calls)
   calls.map do |name|
-    name.
-  # Your code here
+    name.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
