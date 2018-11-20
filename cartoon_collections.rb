aff601f25020b5c0aeea40216fd60ab8cfe27375
Marie Burns <mcburns2222@gmail.com>
2018-11-20 17:31:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheeses.include?(cheese_types)
-     cheeses.find
+     cheeses.find(cheese_types)
   else
   end
 end
