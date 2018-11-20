2a96e6ddb7807ef9dfa62affae12c91c0aa6cad8
Marie Burns <mcburns2222@gmail.com>
2018-11-20 17:10:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
-   if cheeses.include?(cheese_types)
-     puts cheeses.find
+   if cheeses.include?(cheese_types) do |cheese|
+     puts cheese
   else
     nil
   end
